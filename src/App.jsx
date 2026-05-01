64c18ddab4210bbffed1f51edd94f40f83b48fb6
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2026-05-02 00:04:17 +0900

    localStorageでテキストを保存して表示

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,28 @@ import { useState } from "react";
 
 export default function Text() {
   const [text, setText] = useState("");
+  const [savedText, setSavedText] = useState(() => {
+    return localStorage.getItem("memo");
+  });
 
   return (
     <div>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
-      <p>{text}</p>
+      <input
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+
+      <button
+        onClick={() => {
+          localStorage.setItem("memo", text);
+          setSavedText(text);
+        }}
+      >
+        保存
+      </button>
+      <p>{savedText}</p>
     </div>
   );
 }
